cc328c92e6cbcf2989ec4093e602f6abe9348c1e
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-12 20:26:34 +0600

    update

--- moviewala/home/views.py
+++ moviewala/home/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def django(request):
-    return HttpResponse('welcome to django')
+    return HttpResponse('welcome to MovieWala')
+
+
+def home_page(request):
+    return HttpResponse('Home Page')
