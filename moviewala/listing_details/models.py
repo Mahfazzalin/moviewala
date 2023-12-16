7bdd06e6578a046d77dffcbe58dcc06771f68205
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-17 01:24:42 +0600

    create a model on listing_details app

--- moviewala/listing_details/models.py
+++ moviewala/listing_details/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class movie_details(models.Model):
+    name = models.CharField(max_length=200)
+    cover_link = models.URLField(max_length=500)
+    language = models.CharField(max_length=25)
+    subtitle = models.CharField(max_length=30)
+    description = models.TextField()
+    drive_link = models.URLField(max_length=500)
+    screenshot_1 = models.URLField(max_length=500)
+    screenshot_2 = models.URLField(max_length=500)
+    screenshot_3 = models.URLField(max_length=500)
+    screenshot_4 = models.URLField(max_length=500)
