4308de56a881b41ebb729f959d009af7f81873a8
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-17 22:32:23 +0600

    add a row

--- moviewala/listing_details/views.py
+++ moviewala/listing_details/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listing_details.models import movie_details
+
+
 # Create your views here.
 
 
 def hindi(request):
     return HttpResponse('here will show all the hindi movie')
+
+
+def movie_method(request):
+    mov_method = movie_details.object.all()
+    return render(request, 'listing_details/list.html', {'movie': mov_method})
