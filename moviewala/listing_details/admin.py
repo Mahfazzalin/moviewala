51696d3cbe0acefe9a2f099d55a605fb9d78e2a4
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-19 12:07:50 +0600

    update model class for changing database colomn name.

--- moviewala/listing_details/admin.py
+++ moviewala/listing_details/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from listing_details.models import movie_details
+
+
 # Register your models here.
-admin.site.register(movie_details)
+class movie_detailsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cover_link', 'language', 'subtitle', 'textbox', 'd_link', 'ss_1', 'ss_2', 'ss_3', 'ss_4')
+
+
+admin.site.register(movie_details, movie_detailsAdmin)
