7bd70ba2229c3b799d0bd09a5ffbeaa0ae487a27
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-20 14:40:51 +0600

    update

--- moviewala/home/forms.py
+++ moviewala/home/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class MovieRequest(forms.Form):
     email = forms.EmailField()
     subject = forms.CharField(max_length=150)
-    MovieName = forms.Textarea()
+    MovieName = forms.CharField(max_length=500)
