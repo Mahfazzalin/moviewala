dd34190bd4bcf5e56b8d9aea672009f8766e5041
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-20 14:21:38 +0600

    create a view for forms.py

--- moviewala/home/views.py
+++ moviewala/home/views.py
@@ -1,20 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
+from home.forms import MovieRequest
 
 
 # Create your views here.
 
 def home_page(request):
-
     return render(request, 'home/home.html')
 
 
 def bk(request):
-
     return render(request, 'home/bkash/bkash.html')
 
 
 def add(request):
-
     return render(request, 'home/common/base.html')
+
+
+def movie_request(request):
+    frm = MovieRequest()
+    return render(request, 'home/MovieRequestForm.html', {'form': frm})
+
