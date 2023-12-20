d13695c985ddc848aaf4cde75ce02aa26c3c269f
mahfazzalin <mahfazzalin1@gmail.com>
2023-12-20 14:27:26 +0600

    create a view for forms.py

--- moviewala/home/urls.py
+++ moviewala/home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('bkash/', views.bk, name='bkash'),
     path('math/', views.add),
+    path('movie_request/', views.movie_request)
 ]
