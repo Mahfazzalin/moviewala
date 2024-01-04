34902b5fb3021b9fe21ea879834c6fde34bb854e
mahfazzalin <mahfazzalin1@gmail.com>
2024-01-04 23:15:13 +0600

    update

--- moviewala/home/forms.py
+++ moviewala/home/forms.py
@@ -5,5 +5,5 @@ class MovieRequest(forms.Form):
     From = forms.EmailField(initial='mahfazzalin1@gmal.com', disabled=True)
     To = forms.EmailField(label='Enter your Email', label_suffix=' ➡️')
     subject = forms.CharField(max_length=20, min_length=6, error_messages={'required':'Enter a Subject Name'})
-    Moviename = forms.CharField(max_length=500, help_text='Enter your movie name', widget=forms.Textarea)
+    Moviename = forms.CharField(max_length=500, help_text='Enter your movie details', widget=forms.Textarea)
     #checkbox = forms.CharField(widget=forms.CheckboxInput)
