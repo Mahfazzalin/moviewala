abdba0fd544dccc28c482f48f2a199037518651b
mahfazzalin <mahfazzalin1@gmail.com>
2024-01-04 23:08:48 +0600

    update

--- moviewala/home/forms.py
+++ moviewala/home/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class MovieRequest(forms.Form):
     From = forms.EmailField(initial='mahfazzalin1@gmal.com', disabled=True)
     To = forms.EmailField(label='Enter your Email', label_suffix=' ➡️')
-    subject = forms.CharField(max_length=150, widget=forms.PasswordInput)
+    subject = forms.CharField(widget=forms.PasswordInput, max_length=20, min_length=6, error_messages={'required':'Enter your password'})
     Moviename = forms.CharField(max_length=500, help_text='Enter your movie name', widget=forms.Textarea)
     checkbox = forms.CharField(widget=forms.CheckboxInput)
